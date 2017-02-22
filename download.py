3da57559a879265881edfb489ef13794e3faa486
mbuckler <markabuckler@gmail.com>
2017-02-21 22:48:54 -0500

    Successful parallel downloads

--- download.py
+++ download.py
@@ -20,10 +20,10 @@
 from __future__ import unicode_literals
 import imageio
 from ffmpy import FFmpeg
-#imageio.plugins.ffmpeg.download()
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from subprocess import check_call
 from concurrent import futures
+from random import shuffle
+import subprocess
 import youtube_dl
 import socket
 import os
@@ -32,17 +32,19 @@ import sys
 import csv
 
 # The data sets to be downloaded
+#d_sets = ['yt_bb_classification_validation']
 d_sets = ['yt_bb_classification_train',
           'yt_bb_classification_validation',
           'yt_bb_detection_train',
           'yt_bb_detection_validation']
+
 
 # Host location of segment lists
 web_host = 'https://research.google.com/youtube-bb/'
 
 # Video clip class
 class video_clip(object):
-  def __init__(self,name,yt_id,start,stop,class_id,obj_id):
+  def __init__(self,name,yt_id,start,stop,class_id,obj_id,d_set_dir):
     # name = yt_id+class_id+object_id
     self.name     = name
     self.yt_id    = yt_id
@@ -50,6 +52,7 @@ class video_clip(object):
     self.stop     = stop
     self.class_id = class_id
     self.obj_id   = obj_id
+    self.d_set_dir = d_set_dir
   def print_all(self):
     print('['+self.name+', '+ \
               self.yt_id+', '+ \
@@ -59,51 +62,41 @@ class video_clip(object):
               self.obj_id+']\n')
 
 # Download and cut a clip to size
-def dl_and_cut(clip,d_set_dir):
-  # Inform user of progress
+def dl_and_cut(clip):
+
+  d_set_dir = clip.d_set_dir
 
   # Make the class directory if it doesn't exist yet
   class_dir = d_set_dir+'/'+str(clip.class_id)
   check_call(['mkdir', '-p', class_dir])
 
-  # Use youtube_dl to download the video
-  ydl_opts = {
-    # Choose the best quality available
-    'format': 'best[ext=mp4]',
-    'outtmpl': d_set_dir+'/'+clip.name+'_temp.%(ext)s'
-  }
-  ydl = youtube_dl.YoutubeDL(ydl_opts)
+# Create a temporary folder for this download to proceed in
+  check_call(['mkdir', '-p', d_set_dir+'/'+clip.name+'/'])
 
-  try:
-    ydl.download(['http://youtu.be/'+clip.yt_id])
-  # If a dead link, skip
-  except youtube_dl.utils.DownloadError:
-    return 
-  # If timed out, retry one more time
-  except socket.error:
-    try:
-        ydl.download(['http://youtu.be/'+clip.yt_id])
-    except youtube_dl.utils.DownloadError:
-      return
-    # If timed out twice, skip
-    except socket.error:
-      return 
+  # Use youtube_dl to download the video
+  FNULL = open(os.devnull, 'w')
+  check_call(['youtube-dl', \
+    #'--no-part', \
+    '--no-progress', \
+    '-f','best[ext=mp4]', \
+    '-o',d_set_dir+'/'+clip.name+'/'+clip.name+'_temp.mp4', \
+    'youtu.be/'+clip.yt_id ], \
+     stdout=FNULL,stderr=subprocess.STDOUT )
 
   # Verify that the file has been downloaded. Skip otherwise
-  if os.path.exists(d_set_dir+'/'+clip.name+'_temp.mp4'):
+  if os.path.exists(d_set_dir+'/'+clip.name+'/'+clip.name+'_temp.mp4'):
     # Cut out the clip within the downloaded video and save the clip
     # in the correct class directory. Note that the -ss argument coming
     # first tells ffmpeg to start off with an I frame (no frozen start).
     check_call(['ffmpeg',\
       '-ss', str(float(clip.start)/1000),\
-      '-i','file:'+d_set_dir+'/'+clip.name+'_temp.mp4',\
+      '-i','file:'+d_set_dir+'/'+clip.name+'/'+clip.name+'_temp.mp4',\
       '-t', str((float(clip.start)+float(clip.stop))/1000),\
-      '-c','copy',class_dir+'/'+clip.name+'.mp4'])
+      '-c','copy',class_dir+'/'+clip.name+'.mp4'],
+        stdout=FNULL,stderr=subprocess.STDOUT)
 
     # Remove the temporary video
-    os.remove(d_set_dir+'/'+clip.name+'_temp.mp4')
-
-
+    check_call(['rm','-rf',d_set_dir+'/'+clip.name])
 
 
 # Parse the annotation csv file and schedule downloads and cuts
@@ -141,7 +134,6 @@ def parse_and_sched(dl_dir='videos',num_threads=4):
     current_clip_name = ['blank']
     clips             = []
 
-
     # Parse annotations into list of clips with names, youtube ids, start
     # times and stop times
     for idx, annotation in enumerate(annotations):
@@ -153,7 +145,7 @@ def parse_and_sched(dl_dir='videos',num_threads=4):
       yt_id    = annotation[0]
       class_id = annotation[2]
 
-      clip_name = yt_id+':'+class_id+':'+obj_id
+      clip_name = yt_id+'+'+class_id+'+'+obj_id
 
       # If this is a new clip
       if clip_name != current_clip_name:
@@ -170,7 +162,8 @@ def parse_and_sched(dl_dir='videos',num_threads=4):
           clip_start, \
           '0', \
           class_id, \
-          obj_id ) )
+          obj_id, \
+          d_set_dir) )
 
         # Update the current clip name
         current_clip_name = clip_name
@@ -178,24 +171,17 @@ def parse_and_sched(dl_dir='videos',num_threads=4):
     # Update the final clip with its stop time
     clips[-1].stop = annotations[-1][1]
 
-    # Create the progress file
-    prog_file = open('progress.txt','w')
+    # Shuffle the clips 
+    shuffle(clips)
 
-    # Squash terminal output from downloader and cutter
-    save_stdout = sys.stdout
-    sys.stdout  = io.BytesIO()
- 
-    # Download and cut every clip. Perform in parallel to get more
-    # bandwidth from YouTube
+    # Download and cut in parallel threads giving 
     with futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-      fs = [executor.submit(dl_and_cut,clip,d_set_dir) for clip in clips]
+      fs = [executor.submit(dl_and_cut,clip) for clip in clips]
       for i, f in enumerate(futures.as_completed(fs)):
         # Write progress to error so that it can be seen
-        sys.stderr.write("Clip: {} / {} \r".format(i, len(clips)))
-        
-    # Resume terminal output
-    sys.stdout = save_stdout
-
+        sys.stderr.write( \
+          "Downloaded clip: {} / {} \r".format(i, len(clips)))
+    
     print( d_set+': All clips downloaded' )
 
 if __name__ == '__main__':
